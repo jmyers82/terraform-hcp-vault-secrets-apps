341403338b763c566603d1a6f10e57177accc39a
jeremymyers <jeremy@jeremy.com>
2024-08-15 22:34:48 -0600

    fix regex

--- variables.tf
+++ variables.tf
@@ -19,9 +19,11 @@ variable "app_name" {
     error_message = "The app name must contain only alphanumeric characters and dashes"
   }
   validation {
-    condition     = can(regex("^[a-zA-Z]$", var.app_name))
-    error_message = "The app name must start with an alphanumeric character"
+    condition     = can(regex("^[a-zA-Z].$", var.app_name))
+    error_message = "The app name must start with an alpha character"
   }
+
+
 
 }
 
