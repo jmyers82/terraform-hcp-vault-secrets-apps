c78eb6f391e208056360601f7cc9cae46d30f037
jeremy myers <rockymtnskier@gmail.com>
2024-09-21 16:55:36 -0600

    add tf resources

---
+++ tf_resources.tf
@@ -0,0 +1,50 @@
+
+
+
+resource "tfe_project" "app_specific" {
+  organization = var.tfe_organization
+  name         = lower("${var.project_name}-${var.app_name}-app-mgr")
+}
+
+resource "tfe_variable_set" "app_specific_var_set" {
+  name         = lower("${var.project_name}-${var.app_name}-app-mgr")
+  description  = "Auth Vars for ${var.project_name} vault secrets app."
+  organization = var.tfe_organization
+}
+
+resource "tfe_project_variable_set" "app_specific_var_set" {
+  variable_set_id = tfe_variable_set.app_specific_var_set.id
+  project_id      = tfe_project.app_specific.id
+}
+
+resource "tfe_variable" "app_specific_client_id" {
+  key             = "HCP_CLIENT_ID"
+  value           = hcp_service_principal_key.app_spn_key.client_id
+  category        = "env"
+  description     = "Client ID for the HCP Service Principal"
+  variable_set_id = tfe_variable_set.app_specific_var_set.id
+}
+
+resource "tfe_variable" "app_specific_client_secret" {
+  key             = "HCP_CLIENT_SECRET"
+  value           = hcp_service_principal_key.app_spn_key.client_secret
+  category        = "env"
+  description     = "HCP Service Principal Client Secret"
+  variable_set_id = tfe_variable_set.app_specific_var_set.id
+}
+
+resource "tfe_variable" "app_specific_organization_id" {
+  key             = "ORGANIZATION_ID"
+  value           = data.tfe_organization.tf_org.id
+  category        = "env"
+  description     = "HCP Organization ID"
+  variable_set_id = tfe_variable_set.app_specific_var_set.id
+}
+
+resource "tfe_variable" "app_specific__id" {
+  key             = "PROJECT_ID"
+  value           = tfe_project.app_specific.id
+  category        = "env"
+  description     = "PROJECT_ID"
+  variable_set_id = tfe_variable_set.app_specific_var_set.id
+}
